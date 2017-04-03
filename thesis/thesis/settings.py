084533d15ced1a560cca547fe01cbadce14ae3c5
Rizzah <rizzah_2113@yahoo.com>
2017-04-03 12:35:08 +0800

    no message

--- thesis/thesis/settings.py
+++ thesis/thesis/settings.py
@@ -125,3 +125,10 @@ USE_TZ = True
 STATIC_ROOT = BASE_DIR + '/webapp/static'
 
 STATIC_URL = '/static/'
+
+BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'Africa/Nairobi'
