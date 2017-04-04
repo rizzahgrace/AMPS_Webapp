734271913f2287e1d09f3e0de04111f7214d5a8b
Rizzah <rizzah_2113@yahoo.com>
2017-04-05 07:02:21 +0800

    Trying out the scheduler

--- thesis/thesis/settings.py
+++ thesis/thesis/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+from datetime import timedelta
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -27,7 +28,20 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['192.168.33.10', '127.0.0.1', 'localhost']
 
+BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'Africa/Nairobi'
 
+# CELERYBEAT_SCHEDULE = {
+#     'test_print': {
+#         'task': 'display',
+#         'schedule': timedelta(minutes=1),
+#         'args': (data.id),
+#     },
+# }
 # Application definition
 
 INSTALLED_APPS = [
@@ -126,9 +140,3 @@ STATIC_ROOT = BASE_DIR + '/webapp/static'
 
 STATIC_URL = '/static/'
 
-BROKER_URL = 'redis://localhost:6379'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
-CELERY_ACCEPT_CONTENT = ['application/json']
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_SERIALIZER = 'json'
-CELERY_TIMEZONE = 'Africa/Nairobi'
