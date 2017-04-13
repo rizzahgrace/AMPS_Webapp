671db741305725ca8c661e870d7327cd72144f22
Rizzah <rizzah_2113@yahoo.com>
2017-04-13 21:02:20 +0800

    no message

--- thesis/webapp/forms.py
+++ thesis/webapp/forms.py
@@ -26,10 +26,10 @@ class recordUser(forms.Form):
             user = User.objects.get(username__iexact=self.cleaned_data['username'])
         except User.DoesNotExist:
             return self.cleaned_data['username']
-        raise forms.ValidationError(_("The username already exists. Please try another one."))
+        raise forms.ValidationError(("The username already exists. Please try another one."))
  
     def clean(self):
         if 'password1' in self.cleaned_data and 'password2' in self.cleaned_data:
             if self.cleaned_data['password1'] != self.cleaned_data['password2']:
-                raise forms.ValidationError(_("The two password fields did not match."))
+                raise forms.ValidationError(("The two password fields did not match."))
         return self.cleaned_data
--- thesis/webapp/tasks.py
+++ thesis/webapp/tasks.py
@@ -3,15 +3,16 @@ from celery.decorators import periodic_task
 from celery.utils.log import get_task_logger
 from celery import task
 from webapp.models import RawData_AMPS
+import urllib
 
 logger = get_task_logger(__name__)
 
 
 @task(name="display")
 def power_data(x):
-	latestdata=RawData_AMPS.objects.get(id = x).id
-	latestdata+=1
-	return latestdata
+	url = "http://192.168.1.112"
+	f = urllib.urlopen(link)
+	myfile = f.read()
 
 @task(name="add_numbers")
 def add(x,y): 
